1a64d3b329d5e6191b0a3ab73ec72bf728af7838
ihsanemirkuzucu <ihsanemirkuzucu44@gmail.com>
2023-03-03 01:51:02 +0300

    Projenin son hali.

--- NLayer.API/Modules/RepoServiceModule.cs
+++ NLayer.API/Modules/RepoServiceModule.cs
@@ -17,8 +17,13 @@ namespace NLayer.API.Modules
         protected override void Load(ContainerBuilder builder)
         {
             builder.RegisterGeneric(typeof(GenericRepository<>)).As(typeof(IGenericRepository<>)).InstancePerLifetimeScope();
-            builder.RegisterGeneric(typeof(Service<>)).As(typeof(IService<>)).InstancePerLifetimeScope();
-            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
+
+            builder.RegisterGeneric(typeof(Service<>)).As(typeof(IServiceWithDto<,>)).InstancePerLifetimeScope();
+            builder.RegisterGeneric(typeof(IServiceWithDto<,>)).As(typeof(IService<>)).InstancePerLifetimeScope();
+
+
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerLifetimeScope();
+            builder.RegisterType<ProductServiceWithDto>().As<IProductServiceWithDto>().InstancePerLifetimeScope();
             var apiAssembly = Assembly.GetExecutingAssembly();
             var repositoryAssembly = Assembly.GetAssembly(typeof(AppDbContext));
             var serviceAssembly = Assembly.GetAssembly(typeof(MapProfile));
